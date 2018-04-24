97a3b412886ff32552baf4f83fde134387e64fe5
AAW\User <ahmad-wachah@hotmail.com>
2018-04-24 12:27:45 +0200

    Admin kontrolpanel

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
+//Express
 const express = require('express');
 const app = express();
 var bodyParser = require('body-parser');
+
 app.use(express.static('public'));
-
-
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
-
 app.set('view engine', 'ejs');
+
+//Mongodb
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID;
+const mongodburl =  'mongodb://mooz321:zbalezbale@cluster0-shard-00-00-fpxig.mongodb.net:27017,cluster0-shard-00-01-fpxig.mongodb.net:27017,cluster0-shard-00-02-fpxig.mongodb.net:27017/ligeGyldig?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+
+
 
 //var Cors = require('cors'); //test
 //app.use(Cors());
 
-const MongoClient = require('mongodb').MongoClient;
-const ObjectId = require('mongodb').ObjectID;
-const mongodburl = 'mongodb://mooz321:zbalezbale@cluster0-shard-00-00-fpxig.mongodb.net:27017,cluster0-shard-00-01-fpxig.mongodb.net:27017,cluster0-shard-00-02-fpxig.mongodb.net:27017/ligeGyldig?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+
 
 app.get('/', function(req, res){
-    res.render('login.ejs');
+    res.render('');
 });
+
+app.get('/login', function(req, res){
+    res.render('login');
+});
+
+app.get('/login/menu-admin', function(req, res){
+    res.render('menu-admin');
+});
+
 
 app.listen(process.env.PORT || 3000);
