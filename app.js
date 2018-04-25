a53f856e3ff03d7f97d0704ab799a3197ed56d79
AAW\User <ahmad-wachah@hotmail.com>
2018-04-25 13:42:43 +0200

    working on admin panel, customize menu. Login done.

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
 //Express
 const express = require('express');
 const app = express();
-var bodyParser = require('body-parser');
-
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(bodyParser.json());
-app.set('view engine', 'ejs');
+const bodyParser = require('body-parser');
+const Cors = require('cors'); //test
 
 //Mongodb
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectID;
 const mongodburl =  'mongodb://mooz321:zbalezbale@cluster0-shard-00-00-fpxig.mongodb.net:27017,cluster0-shard-00-01-fpxig.mongodb.net:27017,cluster0-shard-00-02-fpxig.mongodb.net:27017/ligeGyldig?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
 
-
-
-//var Cors = require('cors'); //test
-//app.use(Cors());
-
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+app.use(Cors());
+app.set('view engine', 'ejs');
 
 
 app.get('/', function(req, res){
@@ -28,8 +24,26 @@ app.get('/login', function(req, res){
     res.render('login');
 });
 
-app.get('/login/menu-admin', function(req, res){
-    res.render('menu-admin');
+app.get('/login/panel-admin', function(req, res){
+    res.render('panel-admin');
+});
+
+app.get('/login/panel-admin/customize-menu', function(req, res){
+    
+    MongoClient.connect(mongodburl, function (err, db) {
+        
+        var col = db.collection('Categories');
+        // Read All
+        col.find().toArray(function (err, result) {
+            //console.log(result);
+            res.render('customize-menu', {Categories: result});
+        });
+        db.close();
+    });
+});
+
+app.get('/login/panel-admin/orders', function(req, res){
+    res.render('orders');
 });
 
 
