27c28d45ebfd8da115e4db80bfc98a60d412c6c8
CMT <cihat_2650@hotmail.com>
2018-04-26 12:24:02 +0200

    database now online

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ const Category = require('./models/categories');
 //Mongodb
 // const MongoClient = require('mongodb').MongoClient;
 // const ObjectId = require('mongodb').ObjectID;
-//const mongodburl =  'mongodb://mooz321:zbalezbale@cluster0-shard-00-00-fpxig.mongodb.net:27017,cluster0-shard-00-01-fpxig.mongodb.net:27017,cluster0-shard-00-02-fpxig.mongodb.net:27017/ligeGyldig?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
-
-mongoose.connect('mongodb://localhost/restaurant_order_app');
+const mongodbURL = 'mongodb://admin:admin@ds159489.mlab.com:59489/restaurant_app'
+// mongoose.connect('mongodb://localhost/restaurant_order_app');
+mongoose.connect(mongodbURL);
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended : true}));
@@ -113,6 +113,8 @@ app.get('/login/panel-admin/customize-menu', function(req, res){
     //     db.close();
     // });
 });
+
+app.post
 
 app.get('/panel-admin/orders', function(req, res){
     res.render('orders');
