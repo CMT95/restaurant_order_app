8391384e80a556bb916b0d0334c389fb99404693
CMT <cihat_2650@hotmail.com>
2018-04-30 11:56:32 +0200

    user assocatian

--- models/categories.js
+++ models/categories.js
@@ -4,6 +4,13 @@ var mongoose = require('mongoose');
 var categorySchema = new mongoose.Schema({
     title: String,
     image: String,
+    shop: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        username: String
+    }
 });
 
 module.exports = mongoose.model('categories', categorySchema);
